77c594127a47f9ce3b0940a98a121348860ddb85
neelanand <neel.r.anand@gmail.com>
2015-10-12 23:17:11 -0700

    Remove console log.

--- app/js/card_service.js
+++ app/js/card_service.js
@@ -5,7 +5,6 @@ angular.module('vurbApp').factory('cardService', ['$q', '$http', function($q, $h
       var deferred = $q.defer();
 
       $http.get('https://gist.githubusercontent.com/helloandrewpark/0a407d7c681b833d6b49/raw/5f3936dd524d32ed03953f616e19740bba920bcd/gistfile1.js').success(function(data, status, headers, config) {
-          console.log(data);
           return deferred.resolve(data);
         }).error(function(data, status, headers, config) {
           return deferred.reject(copy.error(data));
