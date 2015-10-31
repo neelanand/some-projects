e665b37aa64b0476cdd195de4623a99726ca8741
neelanand <neel.r.anand@gmail.com>
2015-10-31 15:03:03 -0700

    Add expressJS, dependencies and server files.

---
+++ kinsaApp/server.js
@@ -0,0 +1,45 @@
+var express    = require('express');        
+var app        = express();                 
+var bodyParser = require('body-parser');
+
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://kinsa:kinsa@ds045714.mongolab.com:45714/kinsaapp');
+
+var Chat = require('../kinsaApp/models/chat.js');
+console.log(Chat);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8000;
+
+// Routes
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.route('/chat')
+    .post(function(req, res) {
+        
+        var chat = new Chat();
+        chat.username = req.body.username;
+        chat.text = req.body.text;
+
+        console.log(chat);
+        // save the bear and check for errors
+        chat.save(function(err) {
+            if (err)
+                res.send(err);
+            console.log(res);
+            res.json({ message: 'Chat created!' });
+        });
+        
+    });
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Listening to ' + port);
