41b4b722a9ceb4039542def11afb4b8edceaf364
neelanand <neel.r.anand@gmail.com>
2015-11-06 11:51:00 -0800

    Remove app.

--- kinsaApp/models/chat.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var ChatSchema = new Schema({
-    username: String,
-    text: String
-});
-
-module.exports = mongoose.model('Chat', ChatSchema);
